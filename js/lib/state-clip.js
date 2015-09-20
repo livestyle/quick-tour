e968e587ca1cbb7c124832321c0e99c34a9d663d
Sergey Chikuyonok <serge.che@gmail.com>
2015-09-20 22:03:09 +0300

    State clip

---
+++ js/lib/state-clip.js
@@ -0,0 +1,31 @@
+/**
+ * State clip simply changes value of `data-qt-state` with given value
+ * on given time
+ */
+'use strict';
+import AbstractElementClip from './abstract-element-clip';
+
+export default class StateClip extends AbstractElementClip {
+	constructor(elem, keyframes) {
+		super(elem);
+		this.keyframes = Object.keys(keyframes)
+		.map(key => {
+			return {
+				time: +key,
+				state: keyframes[key]
+			};
+		})
+		.sort((a, b) => a.time - b.time);
+		this.render(0);
+	}
+
+	duration() {
+		return this.keyframes[this.keyframes.length - 1].time;
+	}
+
+	render(time) {
+		var keyframe = this.keyframes
+		.reduce((prev, frame) => frame.time <= time ? frame : prev, null);
+		this.elem.setAttribute('data-qt-state', keyframe ? keyframe.state : '');
+	}
+}
