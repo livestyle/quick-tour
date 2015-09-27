6133a1ff025c738f8df5cf7265b5c2e09c9d08ce
Sergey Chikuyonok <serge.che@gmail.com>
2015-09-27 13:17:37 +0300

    Code refactoring

--- js/lib/animations.js
+++ js/lib/animations.js
@@ -1,15 +1,15 @@
 'use strict';
 
-import StateClip from './clip/state';
+import SlideToggleClip from './clip/slide-toggle';
 import slide1 from './slides/slide1';
 import slide2 from './slides/slide2';
 import slide3 from './slides/slide3';
 
 export default function(timeline) {
-	timeline.add(new StateClip(timeline.elem, {
-		100: 'slide1',
-		6000: 'slide2',
-		13000: 'slide3'
+	timeline.add(new SlideToggleClip(timeline.elem.querySelectorAll('.qt-slide'), {
+		100: 'qt-slide1',
+		6000: 'qt-slide2',
+		13000: 'qt-slide3'
 	}));
 
 	slide1(timeline);
