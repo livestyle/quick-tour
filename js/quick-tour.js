ca9958344f2494eddece846d2e47653b60bdc2a2
Sergey Chikuyonok <serge.che@gmail.com>
2015-10-01 23:31:49 +0300

    Minor tweaks

--- js/quick-tour.js
+++ js/quick-tour.js
@@ -2,9 +2,20 @@
 
 import Timeline from './lib/timeline';
 import animations from './lib/animations';
+import {toArray} from './lib/utils';
 
 export default function(elem) {
 	var timeline = new Timeline(elem);
 	animations(timeline);
+
+	var replay = () => {
+		timeline.timecode = 0;
+		timeline.play();
+	};
+
+	toArray(timeline.elem.querySelectorAll('.qt-play-again'))
+	.forEach(el => el.addEventListener('click', replay));
+
+	timeline.elem.classList.remove('qt-invisible');
 	return timeline;
 };
